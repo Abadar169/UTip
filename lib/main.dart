ebe4c777ab6080a4938b48bc6742b0dd7a7593a5
Abhishek Badar <ab15.badar@gmail.com>
2024-06-24 13:27:28 +0530

    add style

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -33,26 +33,42 @@ class UTip extends StatefulWidget {
 class _UTipState extends State<UTip> {
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    final style = theme.textTheme.titleMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+      fontWeight: FontWeight.bold,
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('UTip'),
         
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Container(
-            padding: const EdgeInsets.all(20),
-            decoration:  BoxDecoration(
-              color: Colors.deepPurple,
-              borderRadius: BorderRadius.circular(10),
-              
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              decoration:  BoxDecoration(
+                color: theme.colorScheme.inversePrimary,
+                borderRadius: BorderRadius.circular(10),
+                
+              ),
+              child:  Column(
+                children: [
+                    Text('Total Per Person',
+                    style: style,
+                    ),
+                    Text('\$23.99',
+                      style: style.copyWith(
+                        color: theme.colorScheme.onPrimary,
+                        fontSize: theme.textTheme.displaySmall?.fontSize,
+                      ),
+                    ),
+                ],
+              )
             ),
-            child: const Column(
-              children: [
-                  Text('Total Per Person'),
-                  Text('\$23.99'),
-              ],
-            )
           ),
         ],
       ), 
